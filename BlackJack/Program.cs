4e6d118430a95b9dbbff28eaf8ecdc3e0dcb3bed
Elsa Menzel <menzel.elsa@yandex.ru>
2022-09-28 21:56:56 +0300

    исправление недочетов.

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,38 +10,38 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Добро пожаловать.");
+            Console.WriteLine("Добро пожаловать в игру \"Black Jack\".");
             Console.Write("\nУкажите количество карт, которые имеются у Вас на руках: ");
             int CountCard = int.Parse(Console.ReadLine());
             int summ = 0;
 
             Console.WriteLine("\nВалет - J, Дама - Q, Король - K, Туз - T\n");
 
-            for (int i = 1; i <= CountCard; i++)
+            for (int i = 0; i < CountCard; i++)
             {
-                Console.Write($"Введите номинал {i} карты: ");
+                Console.Write($"Введите номинал {i+1} карты: ");
                 string NominalCard = Console.ReadLine().ToUpper();
 
                 bool IsNumber = int.TryParse(NominalCard, out int result);
                 if (IsNumber == true && int.Parse(NominalCard) <= 10)
                 {
                     int NumberNominalCard = Convert.ToInt32(NominalCard);
-                    summ = summ + NumberNominalCard;
+                    summ += NumberNominalCard;
                 }
 
                 switch (NominalCard)
                 {
                     case "J":
-                        summ = summ + 10;
+                        summ += 10;
                         break;
                     case "Q":
-                        summ = summ + 10;
+                        summ += 10;
                         break;
                     case "K":
-                        summ = summ + 10;
+                        summ += 10;
                         break;
                     case "T":
-                        summ = summ + 11;
+                        summ += 11;
                         break;
                 }
             }
